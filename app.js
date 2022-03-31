6e130392be6fef9ff4e1e680220126af4a98c1eb
qiangmouren <2962051004@qq.com>
2022-03-31 13:02:21 +0800

    优化注释

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  * @create: 2022-03-20 12:24:30
  * @author: qiangmouren (2962051004@qq.com)
  * -----
- * @last-modified: 2022-03-30 06:05:44
+ * @last-modified: 2022-03-31 01:02:00
  * -----
  */
 
@@ -27,17 +27,21 @@ const stream = table.createStream({
   fs.existsSync(USERS_DIR) || fs.mkdirSync(USERS_DIR);
   setCookies(await loadCookies()); // 加载帐号
   const courseListData = await getCourseListData(); // 获取课程列表
-  let hasFailed = false;
+  let hasFailed = false; // 是否存在失败的情况
   for (const { courseName, courseLink } of courseListData) {
     queue.add(async () => {
       let workParams;
-      let retryCount = 0;
+      let retryCount = 0; // 防止出现异常拦截。重试三次
       while ((!workParams || !workParams.classId) && retryCount < 3) {
-        if (retryCount > 0) await delay(500);
-        workParams = await getWorkParams(courseLink); // 获取页面参数
+        if (retryCount > 0) {
+          // 重试的时候延迟500ms
+          await delay(500);
+        }
+        workParams = await getWorkParams(courseLink); // 获取课程页面参数
         retryCount++;
       }
 
+      // 获取课程页面参数失败
       if (!workParams) {
         hasFailed = true;
         return;
@@ -52,6 +56,6 @@ const stream = table.createStream({
   await queue.onIdle();
   console.log(colors.green('\n' + LOG_PREFIX + '任务结束' + LOG_PREFIX));
   if (hasFailed) {
-    console.log(colors.red('\n' + LOG_PREFIX + '存在获取与失败的情况，请自行检查' + LOG_PREFIX));
+    console.log(colors.red('\n' + LOG_PREFIX + '存在获取课程失败的情况,请自行检查' + LOG_PREFIX));
   }
 })();
