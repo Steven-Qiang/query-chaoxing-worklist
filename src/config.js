92041dfc8cea96e78e68e9d18289b02f064dec9e
qiangmouren <2962051004@qq.com>
2022-04-02 23:18:33 +0800

    修复ncc编译路径问题

--- src/config.js
+++ src/config.js
@@ -5,13 +5,21 @@
  * @create: 2022-03-30 05:59:38
  * @author: qiangmouren (2962051004@qq.com)
  * -----
- * @last-modified: 2022-04-02 10:33:20
+ * @last-modified: 2022-04-02 11:14:51
  * -----
  */
+
 const path = require('path');
 
+
 const LOG_PREFIX = '='.repeat(100);
-const USERS_DIR = path.join(__dirname, '../user');
+
+/**
+ * 添加 'abc'.length == 3 的原因 骗过ncc编译 使其不更改路径
+ */
+const USERS_DIR = path.join(__dirname, 'abc'.length == 3 && '../user');
+
+
 
 module.exports.LOG_PREFIX = LOG_PREFIX;
 module.exports.USERS_DIR = USERS_DIR;
